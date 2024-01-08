5ef9d9652f95d8260799739d23b4272363638441
Moe-95 <mhammad_fahs@hotmail.com>
2024-01-08 22:47:30 +0200

    change

--- monty.h
+++ monty.h
@@ -36,7 +36,8 @@ char *opcode;
 void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-void push(stack_t **stack, unsigned int line_number);
+void push(stack_t **stack, unsigned int line_number, char *arg);
 void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
 
 #endif
