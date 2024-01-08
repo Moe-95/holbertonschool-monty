aa9a4bf1fb05dbdb50507cced9e25d892fc4f794
Moe-95 <mhammad_fahs@hotmail.com>
2024-01-08 23:20:43 +0200

    123

--- monty.h
+++ monty.h
@@ -35,9 +35,9 @@ typedef struct instruction_s
 char *opcode;
 void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-extern int command_arg;
+
+extern char *command_arg;
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
-void pint(stack_t **stack, unsigned int line_number);
 
 #endif
--- opcode.c
+++ opcode.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include "monty.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include"monty.h"
 
-int command_arg;
+char *command_arg;
 
 /**
  * push - adds a new node at the beginning of a stack
@@ -12,30 +12,23 @@ int command_arg;
  */
 void push(stack_t **stack, unsigned int line_number)
 {
-if (stack == NULL)
+if (!command_arg || (*command_arg && !isdigit(*command_arg)))
 {
-fprintf(stderr, "L%d: stack is NULL\n", line_number);
+fprintf(stderr, "L%d: usage: push integer\n", line_number);
 exit(EXIT_FAILURE);
 }
 
 stack_t *new_node = malloc(sizeof(stack_t));
-if (new_node == NULL)
+if (!new_node)
 {
 fprintf(stderr, "Error: malloc failed\n");
-exit(EXIT_FAILURE);
-}
-
-if (!command_arg || (*command_arg && !isdigit(*command_arg)))
-{
-fprintf(stderr, "L%d: usage: push integer\n", line_number);
-free(new_node);
 exit(EXIT_FAILURE);
 }
 
 new_node->n = atoi(command_arg);
 new_node->prev = NULL;
 
-if (*stack != NULL)
+if (*stack)
 {
 new_node->next = *stack;
 (*stack)->prev = new_node;
@@ -43,7 +36,6 @@ new_node->next = *stack;
 
 *stack = new_node;
 }
-
 
 /**
  * pall - prints all the values on the stack
@@ -56,25 +48,9 @@ stack_t *current = *stack;
 
 (void)line_number;
 
-while (current != NULL)
+while (current)
 {
 printf("%d\n", current->n);
 current = current->next;
 }
-}
-
-/**
- * pint - prints the value at the top of the stack
- * @stack: double pointer to the head of the stack
- * @line_number: line number of the opcode in the file
- */
-void pint(stack_t **stack, unsigned int line_number)
-{
-if (stack == NULL || *stack == NULL)
-{
-fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
-exit(EXIT_FAILURE);
-}
-
-printf("%d\n", (*stack)->n);
 }
