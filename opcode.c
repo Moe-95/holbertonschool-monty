4b95383fca7f77c7cb19dca839faa5fdac1493d2
Moe-95 <mhammad_fahs@hotmail.com>
2024-01-08 21:52:36 +0200

    First try

---
+++ opcode.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * push - adds a new node at the beginning of a stack
+ * @stack: double pointer to the head of the stack
+ * @line_number: line number of the opcode in the file
+ */
+void push(stack_t **stack, unsigned int line_number)
+{
+if (stack == NULL)
+{
+fprintf(stderr, "L%d: stack is NULL\n", line_number);
+exit(EXIT_FAILURE);
+}
+
+stack_t *new_node = malloc(sizeof(stack_t));
+if (new_node == NULL)
+{
+fprintf(stderr, "Error: malloc failed\n");
+exit(EXIT_FAILURE);
+}
+
+if (!isdigit(arg))
+{
+fprintf(stderr, "L%d: usage: push integer\n", line_number);
+free(new_node);
+exit(EXIT_FAILURE);
+}
+
+new_node->n = atoi(arg);
+new_node->prev = NULL;
+
+if (*stack != NULL)
+{
+new_node->next = *stack;
+(*stack)->prev = new_node;
+}
+
+*stack = new_node;
+}
+
+/**
+ * pall - prints all the values on the stack
+ * @stack: double pointer to the head of the stack
+ * @line_number: line number of the opcode in the file
+ */
+void pall(stack_t **stack, unsigned int line_number)
+{
+stack_t *current = *stack;
+
+(void)line_number; /* Unused parameter */
+
+while (current != NULL)
+{
+printf("%d\n", current->n);
+current = current->next;
+}
+}
