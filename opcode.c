b55aae15e86e7c034fc6a98af768a8d3f80eb18a
Moe-95 <mhammad_fahs@hotmail.com>
2024-01-08 21:56:07 +0200

    pint

--- opcode.c
+++ opcode.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include"monty.h"
 
 /**
  * push - adds a new node at the beginning of a stack
@@ -56,4 +57,19 @@ while (current != NULL)
 printf("%d\n", current->n);
 current = current->next;
 }
+}
+/**
+ * pint - prints the value at the top of the stack
+ * @stack: double pointer to the head of the stack
+ * @line_number: line number of the opcode in the file
+ */
+void pint(stack_t **stack, unsigned int line_number)
+{
+if (stack == NULL || *stack == NULL)
+{
+fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+exit(EXIT_FAILURE);
+}
+
+printf("%d\n", (*stack)->n);
 }
